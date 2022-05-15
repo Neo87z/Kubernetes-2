b9544259c9541023d4c90a0c8851a8aeaeac69b4
Neo87z <shehanhoradagoda12@gmail.com>
2022-05-15 12:26:03 -0230

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('App 2 works!')
+})
+
+app.listen(port, () => {
+  console.log(`app r=nn=ing on ${port}`)
+})
